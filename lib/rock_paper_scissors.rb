b236d5b51de58e0be6c264cdc8819ab7025120cb
Daybeling Lugo Blanco <daybeling.lugo15@est.unanleon.edu.ni>
2018-11-20 12:21:33 -0600

    ajustando codigos

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,11 +4,31 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    result = 0
+	    case player1
+	    	when "rock"
+		      puts "HERE"
+		      result = 0 if player2 == "rock"
+		      result = 1 if player2 == "scissors"
+		      result = 2 if player2 == "paper"
+	    	when "paper"
+		      result = 0 if player2 == "paper"
+		      result = 1 if player2 == "rock"
+		      result = 2 if player2 == "scissors"
+	    	when "scissors"
+		      result = 0 if player2 == "scissors"
+		      result = 1 if player2 == "paper"
+		      result = 2 if player2 == "rock"
+		end
+	result == 0 ? "Draw!" : result == 1 ? "Player 1 won!" : "Player 2 won!"
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    if tournament[0][0].is_a?(String)
+      winner(tournament[0],tournament[1])
+    else
+      winner(tournament_winner(tournament[0]), tournament_winner(tournament[1]))
+    end
   end
 
 end
